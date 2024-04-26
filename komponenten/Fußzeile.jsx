e71ce2670458a9a78fccca622133cd1269afa679
PatrickJungmann <136065944+PatrickJungmann@users.noreply.github.com>
2024-04-26 08:56:36 +0200

    Add files via upload

--- komponenten/Fußzeile.jsx
+++ komponenten/Fußzeile.jsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function Fußzeile() {
     return (
         <div className="d-flex fixed-bottom justify-content-between p-2 text-secondary bg-light footer mt-auto">
-            <h6 className="text-center">2024 © Genussdienst GmbH. <br /> DIES IST EIN LERNPROJEKT.</h6>
+            <h6 className="text-center">Genussdienst<br /> DIES IST EIN LERNPROJEKT.</h6>
             <h6 className="text-center">Öffnungszeiten: <br /> Dienstag - Sonntag (Montag Ruhetag) 10:00 - 22:00 Uhr geöffnet</h6>
-            <h6 className="text-center">Telefon: 07231 / 76767 <br /> E-Mail: info@genussdienst.de</h6>
+            <h6 className="text-center">Telefon: 07231 / 00000 <br /> E-Mail: info@genussdienst.de</h6>
             <h6 className="text-center">
                 <Link href="/" className="text-danger text-decoration-none">Impressum</Link>
                 <br />
